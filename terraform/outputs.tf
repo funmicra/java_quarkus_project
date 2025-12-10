c7f5f7959247d960451c84d7c86dcff462952ad0
funmicra <el.funmicra@gmail.com>
2025-12-11 01:55:28 +0200

    .

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "vm_names" {
   value = concat(
-    [for vm in proxmox_vm_qemu.ctrl-plabne : vm.name],
+    [for vm in proxmox_vm_qemu.ctrl-plane : vm.name],
     [for vm in proxmox_vm_qemu.workers : vm.name]
   )
 }
