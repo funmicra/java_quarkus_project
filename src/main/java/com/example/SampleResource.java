7a57fe4d0ec05c6987ab990298f8005590543c7a
funmicra <el.funmicra@gmail.com>
2025-12-11 03:17:45 +0200

    .

---
+++ src/main/java/com/example/SampleResource.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.Map;
+
+@Path("/sample")
+public class SampleResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> sample(@QueryParam("param") String param) {
+		if (param == null) {
+			param = "default";
+        }
+        return Map.of("param", param);
+    }
+}
